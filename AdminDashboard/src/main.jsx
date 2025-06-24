730c907af1e5eb9641ef0b6355ab3108e0ee9f2e
dhananjaymonga <dhananjaymonga@gmail.com>
2025-06-24 23:08:18 +0530

    quiz app

--- AdminDashboard/src/main.jsx
+++ AdminDashboard/src/main.jsx
@@ -7,40 +7,12 @@ import AdminBlog from "./Admin/AdminBlog";
 import PageHistory from "./Admin/History";
 import AdminQuiz from "./Admin/AdminQuiz";
 import AdminLogin from './Admin/AdLogin';
-import { BrowserRouter,createBrowserRouter,RouterProvider } from 'react-router-dom';
 
 import './index.css'
 import App from './App.jsx'
-const router=createBrowserRouter([
-  {
-    path:"/",
-    element:<AdminLogin/>
-  },
-  {
-    path:"/admin", 
-    element:<AdminPannel/>
-  },
-  {
-    path:"/admin/blog", 
-    element:<AdminBlog/>
-  },
-  {
-    path:"/admin/notes", 
-    element:<AdminNotes/>
-  },
-  {
-    path:"/admin/history", 
-    element:<PageHistory/>
-  },
-  {
-    path:"/admin/quiz", 
-    element:<AdminQuiz/>
-  },
- 
-  
-])
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router}/>
+    <App />
   </StrictMode>,
 )
